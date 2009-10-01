c96dcd9146f4abe49511af74af189f13ecf2ebbf
djm <djm>
2009-10-01 09:51:58 +0000

    Mark GENSALT_DEFAULT_LOG2_ROUNDS "final", suggested by ravindranathakila AT gmail.com

--- BCrypt.java
+++ BCrypt.java
@@ -61,7 +61,7 @@ import java.security.SecureRandom;
  */
 public class BCrypt {
 	// BCrypt parameters
-	private static int GENSALT_DEFAULT_LOG2_ROUNDS = 10;
+	private static final int GENSALT_DEFAULT_LOG2_ROUNDS = 10;
 	private static final int BCRYPT_SALT_LEN = 16;
 
 	// Blowfish parameters
